f4449775c6e7da723f09316f1a45f97d384c6b33
Miguel <miguelmayhem92@gmail.com>
2023-08-31 19:31:40 +0200

    push results

---
+++ DeepLearning_topics/README.md
@@ -0,0 +1,40 @@
+# Image Classification using AWS SageMaker
+
+Use AWS Sagemaker to train a pretrained model that can perform image classification by using the Sagemaker profiling, debugger, hyperparameter tuning and other good ML engineering practices. This can be done on either the provided dog breed classication data set or one of your choice.
+
+## Project Set Up and Installation
+This project is done in AWS SageMaker using s3 for data storage and SageMaker studio for training, validation and deployment
+
+## Dataset
+The provided dataset is the dogbreed classification dataset which can be found in the classroom.
+
+![alt text](https://github.com/miguelmayhem92/udacity_projects/blob/DeepLearning_topics/sreenshots/train_breed_dist.jpg)
+
+### Access
+Upload the data to an S3 bucket through the AWS Gateway so that SageMaker has access to the data. 
+
+## Hyperparameter Tuning
+What kind of model did you choose for this experiment and why? Give an overview of the types of parameters and their ranges used for the hyperparameter search
+
+Remember that your README should:
+- Include a screenshot of completed training jobs
+- Logs metrics during the training process
+- Tune at least two hyperparameters
+- Retrieve the best best hyperparameters from all your training jobs
+
+## Debugging and Profiling
+**TODO**: Give an overview of how you performed model debugging and profiling in Sagemaker
+
+### Results
+**TODO**: What are the results/insights did you get by profiling/debugging your model?
+
+**TODO** Remember to provide the profiler html/pdf file in your submission.
+
+
+## Model Deployment
+**TODO**: Give an overview of the deployed model and instructions on how to query the endpoint with a sample input.
+
+**TODO** Remember to provide a screenshot of the deployed active endpoint in Sagemaker.
+
+## Standout Suggestions
+**TODO (Optional):** This is where you can provide information about any standout suggestions that you have attempted.
---
+++ DeepLearning_topics/train_model.py
@@ -0,0 +1,169 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+import logging
+import os
+import sys
+
+import smdebug.pytorch as smd
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def test(model, test_loader, criterion, hook):
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    test_loss = 0
+    running_corrects = 0
+    
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        test_loss += criterion(outputs, labels).item()
+        _, preds = torch.max(outputs, 1)
+        running_corrects += torch.sum(preds==labels.data).item()
+    
+    average_accuracy = running_corrects/len(test_loader.dataset)
+    average_loss = test_loss/len(test_loader.dataset)
+    logger.info(f'Test set: Average loss: {average_loss}, Accuracy: {100*average_accuracy}%')
+    
+
+
+def train(model, train_loader, validation_loader, epochs, criterion, optimizer, hook): 
+    
+    for epoch in range(epochs):
+        hook.set_mode(smd.modes.TRAIN)
+        model.train()
+        
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+        hook.set_mode(smd.modes.EVAL)
+        model.eval()
+        running_corrects = 0
+        
+        ## validation
+        with torch.no_grad():
+            for inputs, labels in validation_loader:
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                _, preds = torch.max(outputs, 1)
+                running_corrects += torch.sum(preds == labels.data).item()
+                
+        total_accuracy = running_corrects / len(validation_loader.dataset)
+        logger.info(f'Validation set: Average accuracy: {100*total_accuracy}%')
+        
+    return model 
+    
+def net():
+    model = models.resnet50(pretrained = True)
+    
+    ### fixing the conv layers
+    for param in model.parameters():
+        param.required_grad = False 
+    
+    num_features = model.fc.in_features
+    num_classes = 133
+    
+    ### network to train
+    model.fc = nn.Sequential(
+        nn.Linear(num_features, 256), 
+        nn.ReLU(),                 
+        nn.Linear(256, 128),
+        nn.ReLU(), 
+        nn.Linear(128, 128),
+        nn.ReLU(),
+        nn.Linear(128,  num_classes),
+        nn.LogSoftmax(dim=1)
+    )
+    return model
+
+def create_data_loaders(datadir, batch_size):
+
+    train_path = os.path.join(datadir, 'train')
+    validation_path = os.path.join(datadir, 'valid')
+    test_path = os.path.join(datadir, 'test')
+    ### some data augmentation and resizing
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(p=0.6),
+              transforms.Resize(256),
+              transforms.Resize((224, 224)),
+              transforms.ToTensor()
+        ]
+    )
+    
+    ## just resizing
+    val_transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ]
+    )
+    
+    ## processing data to datasets, just train and validation
+    train_dataset = torchvision.datasets.ImageFolder(root=train_path, transform=train_transform)    
+    validation_dataset = torchvision.datasets.ImageFolder(root=validation_path, transform=val_transform)
+    test_dataset = torchvision.datasets.ImageFolder(root=test_path, transform=val_transform)
+    
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validation_data_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_data_loader, validation_data_loader, test_data_loader
+
+def main(args):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model=net()
+    model = model.to(device)
+    
+    loss_criteria = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+    
+    ## hook
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+    
+    ## process train and validation data
+    train_data_loader, val_data_loader, test_data_loader = create_data_loaders(datadir=args.data_dir, batch_size=args.batch_size)
+    
+    ## train model and validate
+    model = train(model, train_data_loader, val_data_loader, args.epochs, loss_criteria, optimizer, hook)
+    ## testing
+    test(model, test_data_loader, loss_criteria, hook)
+    
+    ## saving model
+    logger.info("Saving the model")
+    torch.save(model.state_dict(), os.path.join(args.model_dir, 'model.pth'))
+    logger.info("Model saved")
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    
+    parser.add_argument("--batch_size", type=int, default=64, metavar="N", help="train input batch size")
+    parser.add_argument( "--test_batch_size", type=int, default=1000, metavar="N", help="test input batch size")
+    parser.add_argument("--epochs", type=int, default=2, metavar="N", help="number of epochs")
+    parser.add_argument("--lr", type=float, default=0.01, metavar="LR", help="learning rate")
+    parser.add_argument("--data_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"], help="training data path")
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"], help="location to save the model")
+    
+    args=parser.parse_args()
+    
+    main(args)
